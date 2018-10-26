57fcf1885746537ff3daf1d1ba5701dbdf202f38
Leodai <leo_x_dai@manulife.com>
2018-10-26 17:08:19 +0800

    Update

---
+++ BasicAdvanced/Class/Math.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class
+{
+    class Math
+    {
+        public int Value { get; set; }
+
+        // C# 6 新特性，提供lambda给一个语句的方法
+        public int GetSquare() => Value * Value;
+
+        public static int GetSquareOf(int x) => x * x;
+
+        public static double GetPi() => 3.1415926;
+
+        // 参数可以提供默认值
+        public static float GetDivid(int denominator, int numerator = 1) => denominator / numerator;
+    }
+}
---
+++ BasicAdvanced/Class/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using static System.Console;
+
+namespace Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 属性
+            PropertyFunction();
+            // params 关键字
+            ParamsFunction(1, 2, 3);
+
+            ReadLine();
+        }
+
+        public static void PropertyFunction()
+        {
+            WriteLine($"PI is {Math.GetPi()}");
+            WriteLine($"Square of 5 is : {Math.GetSquareOf(5)} ");
+            var math = new Math();
+            math.Value = 6;
+            WriteLine($"Value of Math is : {math.Value}");
+            WriteLine($"Square of Math Value is : {math.GetSquare()}");
+
+            // 带参数名字, 默认值, 并且可以乱序
+            WriteLine($"Divide of Value 5 with default is : {Math.GetDivid(5)}");
+            WriteLine($"Divide of Value 5 with default is : {Math.GetDivid(numerator: 2, denominator: 5)}");
+        }
+
+        // params 关键字
+        public static void ParamsFunction(params int[] array)
+        {
+            foreach (var i in array)
+            {
+                WriteLine($"params item: {i}");
+            }
+        }
+    }
+}
